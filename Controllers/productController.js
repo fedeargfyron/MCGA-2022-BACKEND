ba88f9ab961d7ff3fb37cdf5e1340a22e8d940cb
fedeargfyron <fedemgs15@gmail.com>
2022-09-08 10:12:47 -0300

    Mejora visual del controller

--- Controllers/productController.js
+++ Controllers/productController.js
@@ -1,21 +1,13 @@
 const res = require('express/lib/response');
 const Product = require('../Schemas/productSchema');
 
-const getProducts = async () => {
-    return await Product.find();
-}
+const getProducts = async () => await Product.find();
 
-const getById = async (id) => {
-    return await Product.findById(id);
-}
+const getById = async (id) => await Product.findById(id);
 
-const postProduct = async (body) => {
-    await new Product(body).save();
-}
+const postProduct = async (body) => new Product(body).save();
 
-const deleteProduct = async (id) => {
-    await Product.findByIdAndDelete(id);
-}
+const deleteProduct = async (id) => await Product.findByIdAndDelete(id);
 
 module.exports = {
     getProducts,
